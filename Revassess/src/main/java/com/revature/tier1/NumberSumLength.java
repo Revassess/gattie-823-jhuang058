9b82030fbc6343ad3da583e3b082b58e3b45cec0
Jane Huang <jhuang058@gmail.com>
2020-08-26 15:03:44 -0400

    tier1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,20 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		
+		
+		String s = num+"";
+		char[] c = s.toCharArray();
+		int l = c.length;
+		long sum = 0;
+		
+		for (char digit: c) {
+			 int powerdigit = (int) Math.pow(Character.getNumericValue(digit), l);
+			sum += powerdigit;
+		}
+		
+			
+		return sum == num;
 	}
 }
+
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,24 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
+
+		int sum = 0;
+
+		try {
+			for (int i : arr) {
+				sum += i;
+			}
+
+			return sum;
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
 		return 0;
+		
+	}
+
+	public static void main(String[] args) {
+		int[] ar = { 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80 };
+		IterateAndSum(ar);
 	}
 }
